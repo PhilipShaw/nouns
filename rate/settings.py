33463c1e6b584d5edd812aea5a3810925231d849
Philip Shaw <phil_shaw@mail.com>
2017-05-30 23:39:08 -0400

    commented out embed_video in INSTALLED_APPS of settings.py

--- rate/settings.py
+++ rate/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main',
-    'embed_video',
+    #'embed_video',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
